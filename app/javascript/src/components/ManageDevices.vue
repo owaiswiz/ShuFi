664a61ce133131238943e89edf184a20b1e696f0
Owais <owaiswiz@gmail.com>
2018-07-05 01:59:34 +0530

    Add funnctions to send request for showing/adding/editing/operating on devices

--- app/javascript/src/components/ManageDevices.vue
+++ app/javascript/src/components/ManageDevices.vue
@@ -3,19 +3,17 @@
     <div class="font-heading text-2xl text-indigo-light py-4 mb-4">Manage Devices</div>
     <div v-for="device in devices" v-bind:key="device.id" class="my-8">
       <div v-if="device.loading" class="flex justify-center px-4 py-2">
-        <div class="loader h-5 w-5"></div>
-      </div>
-
+        <div class="loader h-5 w-5"></div> </div> 
       <div v-else class="flex-grow flex items-center justify-between">
         <div v-if="!device.editing" class="text-md  py-2 mx-4">{{ device.name }}</div>
-        <input v-else v-model="device.mac" class="primary-input flex-grow px-4 py-2 mx-4 w-full text-sm"
-          type="text" placeholder="Mac Address">
+        <input v-else v-model="device.mac" class="primary-input flex-grow px-4 py-2 mx-4 w-full text-sm" type="text" placeholder="Mac Address">
 
         <div class="flex items-baseline cursor-pointer text-sm select-none">
-          <a v-show="!device.editing" class="text-green-light hover:text-green-lighter mr-4" @click.prevent="allow(device)">Allow</a>
-          <a v-show="!device.editing" class="text-red-light hover:text-red-lighter mr-4" @click.prevent="deny(device)">Deny</a>
-          <a v-if="device.editing" class="text-blue-light hover:text-blue-lighter mr-4" @click.prevent="device.editing = false">Done</a>
-          <a v-else class="text-blue-light hover:text-blue-lighter mr-4" @click.prevent="device.editing = true">Edit</a>
+          <a v-show="!device.editing" class="text-green-light hover:text-green-lighter mr-4" @click.prevent="changeDeviceStatus(device, 'allow')">Allow</a>
+          <a v-show="!device.editing" class="text-red-light hover:text-red-lighter mr-4" @click.prevent="changeDeviceStatus(device, 'deny')">Deny</a>
+          <a v-show="!device.editing" class="text-blue-light hover:text-blue-lighter mr-4" @click.prevent="device.editing = true">Edit</a>
+          <a v-show="device.editing" class="text-blue-light hover:text-blue-lighter mr-4" @click.prevent="editDeviceMac(device)">Done</a>
+          <a v-show="!device.editing" class="text-red-dark hover:text-red mr-4" @click.prevent="deleteDevice(device)">Delete</a>
         </div>
       </div>
     </div>
@@ -24,21 +22,54 @@
 
 <script>
 export default {
-  props: ['devices'],
-
   methods: {
-    allow(device) {
-      console.log(device)
+    changeDeviceStatus(device, operation) {
       device.loading = true
-      setTimeout(() => device.loading = false, 1000)
+      this.$http.post(`/devices/${operation}`, { id: device.id })
+        .then(() => {
+          console.log(`Device ${operation}ed`)
+        })
+        .finally(() => device.loading = false)
     },
-    deny(device) {
+
+    editDeviceMac(device) {
+      device.editing = false
       device.loading = true
-      setTimeout(() => device.loading = false, 2000)
+      this.$http.post('/devices/edit', { id: device.id, mac: device.mac })
+        .then(() => {
+          console.log('Device edited')
+        })
+        .finally(() => device.loading = false)
     },
-    edit(device) {
-      device.editing = !device.editing
+
+    deleteDevice(device) {
+      device.loading = true
+      this.$http.post('/devices/delete', { id: device.id })
+        .then(() => {
+          console.log('Device deleted')
+        })
+        .finally(this.updateDevices)
     },
+
+    updateDevices() {
+      this.$http.get('/devices/show').then( resp => {
+        this.devices = resp.body.devices.map(device => {
+          return {...device, editing: false, loading: false}
+        })
+      }) 
+    }
   },
+
+
+  data() {
+    return {
+      devices: []
+    }
+  },
+
+  created() {
+    this.updateDevices()
+    this.$root.$on('update-devices', this.updateDevices)
+  }
 }
 </script>
