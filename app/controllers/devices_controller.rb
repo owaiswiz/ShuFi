debb69c664bfaf510a2590035b9ee103f3903956
Owais <owaiswiz@gmail.com>
2018-07-05 01:58:29 +0530

    Add implementation for adding,showing,editing, allowing denying devices

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -2,19 +2,47 @@ class DevicesController < ApplicationController
   def add
     device = Device.new(name: params[:name], mac: params[:mac])
     if device.save
-      status = 'Device added successfully'
+      render json: { id: device.id}
     else
-      status = 'Error occurred while adding device'
+      head :error
     end
-    render json: { status: status, id: device.id}
   end
 
   def allow
+    change_device_status(id: params[:id], allowed: true)
   end
 
   def deny
+    change_device_status(id: params[:id], allowed: false)
   end
 
   def edit
+    device = Device.find_by(id: params[:id])
+    if device && device.update_attributes(device_params)
+      render json: { id: device.id }
+    else
+      head :error
+    end
   end
+
+  def show
+    render json: { devices: Device.all }
+  end
+
+
+  private
+
+  def change_device_status(id:, allowed:) 
+    device = Device.find_by(id: id)
+    if device and device.update_attributes(allowed: allowed)
+      render json: { id: device.id }
+    else
+      head :error
+    end
+  end
+
+  def device_params
+    params.permit(:name, :mac).reject { |_, v| v.blank? }
+  end
+
 end
