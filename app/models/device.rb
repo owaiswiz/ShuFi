d5969d4b3e71b4311b8f1e615e18e8c29b9d5161
Owais <owaiswiz@gmail.com>
2018-07-05 19:52:35 +0530

    Connect to router and execute appropriate command

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,10 +1,14 @@
+require 'net/ssh'
+
 class Device < ApplicationRecord
   validates :name, :mac, presence: true
   before_update :update_status_on_router, if: :will_save_change_to_allowed?
 
   def update_status_on_router
-    if self.allowed
-
+    return if Rails.env == 'test'
+    Net::SSH.start('192.168.1.1', 'root') do | ssh | 
+      ssh.exec! change_config_file_command("/etc/config/wireless")
+      ssh.exec! 'wifi'
     end
   end
 
