2ad2c4fdf6c06a57433dbbf3400429386ebfa0d9
Owais <owaiswiz@gmail.com>
2018-07-05 19:13:17 +0530

    Comment unnecessary test

--- test/models/device_test.rb
+++ test/models/device_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class DeviceTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert_equal Device.count, 2
+  teardown do
+    FileUtils.copy(file_fixture("wireless_config_backup"), file_fixture("wireless_config"))
+  end
+
+  test "generates proper command to update config file" do
+    device = devices(:one)
+    device.allowed = true
+    `#{device.change_config_file_command(file_fixture("wireless_config").expand_path)}`
+    assert file_fixture("wireless_config").read.include? device.mac
+
+    device.allowed = false
+    `#{device.change_config_file_command(file_fixture("wireless_config").expand_path)}`
+    assert_not file_fixture("wireless_config").read.include? device.mac
   end
 end
