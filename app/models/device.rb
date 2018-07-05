adc02bffb5eda36e041bccc420911f49d4c9bd95
Owais <owaiswiz@gmail.com>
2018-07-05 19:14:21 +0530

    Add implementation to generate proper commands to execute over ssh

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,3 +1,18 @@
 class Device < ApplicationRecord
   validates :name, :mac, presence: true
+  before_update :update_status_on_router, if: :will_save_change_to_allowed?
+
+  def update_status_on_router
+    if self.allowed
+
+    end
+  end
+
+  def change_config_file_command file_path
+    if self.allowed
+      "sed -i '/option macfilter/a list maclist #{self.mac}' #{file_path}"
+    else
+      "sed -i '/#{self.mac}/d' #{file_path}"
+    end
+  end
 end
